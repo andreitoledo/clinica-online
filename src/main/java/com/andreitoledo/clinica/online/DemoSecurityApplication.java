70a75236bf8336a64941989950769ef5e3f0b0ae
andreitoledo <andreiltoledo@hotmail.com>
2023-03-24 22:06:15 -0300

    Adicionando usuario adm no banco de dados

--- src/main/java/com/andreitoledo/clinica/online/DemoSecurityApplication.java
+++ src/main/java/com/andreitoledo/clinica/online/DemoSecurityApplication.java
@@ -2,13 +2,13 @@ package com.andreitoledo.clinica.online;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DemoSecurityApplication {
 
 	public static void main(String[] args) {
-//		System.out.println(new BCryptPasswordEncoder().encode("root"));
+	//	System.out.println(new BCryptPasswordEncoder().encode("Admin@123"));
 		SpringApplication.run(DemoSecurityApplication.class, args);
 	}
 }
