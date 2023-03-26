e22783bd261cf8a0c4c2b41fd1695edebaaf91e4
andreitoledo <andreiltoledo@hotmail.com>
2023-03-26 16:11:39 -0300

    Tratando exceção de usuário não encontrado e alguns ajustes no design

---
+++ src/main/java/com/andreitoledo/clinica/online/web/Exception/ExceptionController.java
@@ -0,0 +1,28 @@
+package com.andreitoledo.clinica.online.web.Exception;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/*essa anotação serve como um ouvinte na aplicação. Fica esperando
+que algumas dessas regras sejam verdadeiras, quando forem, essa classe
+ativa o método.
+
+As regras criadas aqui, são regras para capturar Exception/exceção.
+*/
+
+@ControllerAdvice
+public class ExceptionController {
+	
+	// essa anotação define a exceção que queremos tratar no método.
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public ModelAndView usuarioNaoEncontradoException(UsernameNotFoundException ex) {
+		ModelAndView model = new ModelAndView("error");
+		model.addObject("status", 404);
+		model.addObject("error", "Operação não pode ser realizada.");
+		model.addObject("message", ex.getMessage());
+		return model;
+	}
+
+}
