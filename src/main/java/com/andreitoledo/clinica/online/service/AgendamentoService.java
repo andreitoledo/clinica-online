ab99b0c8631478982d392ef0bcde55db6df77310
andreitoledo <andreiltoledo@hotmail.com>
2023-04-01 01:22:18 -0300

    Agendamento - Selecionando a data e hora da Consulta

--- src/main/java/com/andreitoledo/clinica/online/service/AgendamentoService.java
+++ src/main/java/com/andreitoledo/clinica/online/service/AgendamentoService.java
@@ -1,9 +1,26 @@
 package com.andreitoledo.clinica.online.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.andreitoledo.clinica.online.domain.Horario;
+import com.andreitoledo.clinica.online.repository.AgendamentoRepository;
 
 @Service
 public class AgendamentoService {
+	
+	@Autowired
+	private AgendamentoRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<Horario> buscarHorariosNaoAgendadosPorMedicoIdEData(Long id, LocalDate data) {
+		
+		return repository.findByMedicoIdAndDataNotHorarioAgendado(id, data);
+	}
 
 	
 }
