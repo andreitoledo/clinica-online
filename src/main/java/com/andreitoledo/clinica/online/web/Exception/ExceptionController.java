d63fdcc6238ddac189069458618f0aed72eed1b4
andreitoledo <andreiltoledo@hotmail.com>
2023-04-02 16:14:39 -0300

    Tratando exceção de acesso negado

--- src/main/java/com/andreitoledo/clinica/online/web/Exception/ExceptionController.java
+++ src/main/java/com/andreitoledo/clinica/online/web/Exception/ExceptionController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.andreitoledo.clinica.online.exception.AcessoNegadoException;
+
 /*essa anotação serve como um ouvinte na aplicação. Fica esperando
 que algumas dessas regras sejam verdadeiras, quando forem, essa classe
 ativa o método.
@@ -20,6 +22,15 @@ public class ExceptionController {
 	public ModelAndView usuarioNaoEncontradoException(UsernameNotFoundException ex) {
 		ModelAndView model = new ModelAndView("error");
 		model.addObject("status", 404);
+		model.addObject("error", "Operação não pode ser realizada.");
+		model.addObject("message", ex.getMessage());
+		return model;
+	}
+		
+	@ExceptionHandler(AcessoNegadoException.class)
+	public ModelAndView acessoNegadoException(AcessoNegadoException ex) {
+		ModelAndView model = new ModelAndView("error");
+		model.addObject("status", 403);
 		model.addObject("error", "Operação não pode ser realizada.");
 		model.addObject("message", ex.getMessage());
 		return model;
